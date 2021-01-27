c1ce32c92bedd06dc722d9da4074196b894323a5
Michał <mmajchrzak1@gmail.com>
2021-01-27 16:44:36 +0100

    Add get_movies and get_series

--- moviesLibrary.py
+++ moviesLibrary.py
@@ -11,11 +11,24 @@ class Movie:
     def __str__(self):
         return f"{self.title} ({self.release_year})"
 
+
 class TVSeries(Movie):
-    def __init__(self, season_no, episode_no, *args, **kwargs)
+    def __init__(self, season_no, episode_no, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.season_no = season_no
         self.episode_no = episode_no
 
     def __str__(self):
         return f"{self.title} S{self.season_no}E{self.episode_no}"
+
+
+def get_movies(library):
+    movies_list = [movie for movie in library if not isinstance(movie, TVSeries)]
+    sorted_list = sorted(movies_list, key=lambda movie: movie.title)
+    return sorted_list
+
+
+def get_series(library):
+    tv_series_list = [series for series in library if isinstance(series, TVSeries)]
+    sorted_list = sorted(tv_series_list, key=lambda series: series.title)
+    return sorted_list
